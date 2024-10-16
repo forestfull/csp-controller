49fc7ea67ddccd3afe0dd25bea411812848ccfe1
hs.ko <tpsxja)!(@>
2024-10-16 12:59:30 +0900

    템플릿 생성 템플릿 엔진 제거 컨트롤러 초기화시 읽어오도록 설정

---
+++ src/main/java/com/cs21/csp/CspController.java
@@ -0,0 +1,55 @@
+package com.cs21.csp;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class CspController {
+
+    private final String indexContents;
+
+    public CspController() {
+        final StringBuilder sb = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            ClassPathResource classPathResource = new ClassPathResource("index.html");
+            reader = new BufferedReader(new InputStreamReader(classPathResource.getInputStream()));
+            reader.lines().forEach(sb::append);
+
+        } catch (IOException ignored) {
+        } finally {
+            try {
+                reader.close();
+            } catch (Exception ignored) {
+            }
+        }
+
+        this.indexContents = sb.toString();
+    }
+
+    @GetMapping("/_Admin/csp.json")
+    String goCspJson(HttpServletRequest request) {
+        return indexContents;
+    }
+
+    @PostMapping("/_Admin/csp.json")
+    String getCspJson(HttpServletRequest request) throws JsonProcessingException {
+        final List<CsCspResources> resources = CspResponseFilter.getService().getAll();
+        if (resources == null) {
+            return "";
+        } else {
+            return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(resources.stream().map(CsCspResources::convertMap).collect(Collectors.groupingBy(map -> map.get("target"))));
+        }
+    }
+}
